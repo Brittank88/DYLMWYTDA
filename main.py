f1560fbe9642a95457a8e7a4d5e2b9dba7817347
Mason Hunt <masonhunt130ii@gmail.com>
2022-01-28 18:47:29 +1100

    Concurrency update!

--- main.py
+++ main.py
@@ -3,18 +3,25 @@
 #--------------------------#
 # Created By  : Brittank88
 # Created Date: 28/01/2022
-# Version     : '1.0.0'
+# Version     : '2.0.0'
 # -------------------------#
 """Python script to download all the SFX from https://gdcolon.com/🗿."""
 
 from argparse import ArgumentParser
 from json import loads
+from xmlrpc.client import Boolean
 from requests import get
 from pathlib import Path
 from tqdm import tqdm
+from tqdm.contrib.concurrent import thread_map
+from loguru import logger
+from os import cpu_count
+from threading import get_native_id
+from colorama import Fore, Back, Style
 
 BLOCK_SIZE = 1024   # 1 Kibibyte = 1024 bytes
 DOMAIN     = 'https://gdcolon.com'
+BLOCKING_COEFFICIENT = 0.5
 
 def init_argparse() -> ArgumentParser:
     """Initialises and returns the argument parser."""
@@ -25,13 +32,21 @@ def init_argparse() -> ArgumentParser:
     )
     parser.add_argument(
         '-o', '--output'                 ,
-        help    = 'The output directory.',
-        default = './sfx'
+        help    = 'The output directory for downloaded SFX.',
+        default = './sfx'                                   ,
+        type    = str
     )
     parser.add_argument(
         '-s', '--soundlist'                                    ,
         help    = 'The JSON soundlist url (excluding domain!).',
-        default = '/server/soundlist'
+        default = '/server/soundlist'                          ,
+        type    = str
+    )
+    parser.add_argument(
+        '-t', '--threads'                                  ,
+        help    = 'The number of threads to use.'          ,
+        default = cpu_count() // (1 - BLOCKING_COEFFICIENT),
+        type    = int
     )
     return parser
 
@@ -39,23 +54,31 @@ def main() -> None:
     """Main function to download SFX from Colon's server."""
 
     parser = init_argparse()
-    args = parser.parse_args()
+    args   = parser.parse_args()
 
-    for i, sfx_entry in enumerate(
-        loads(get(f'{DOMAIN}/{args.soundlist}').text)
-    ):
-        filename = sfx_entry.get('filename', f'sfx_{i}.wav')
-        filename_ext = filename.split('/')[-1]
+    logger.remove()
+    logger.add(lambda msg: tqdm.write(msg, end = ''))
 
-        filepath = Path(args.output, filename_ext)
-        filepath.parents[0].mkdir(parents = True, exist_ok = True)
+    def _download(sound: dict) -> None:
+        """Underlying function that ownloads a sound from the server."""
 
-        sfx_file = get(f'{DOMAIN}/{filename}', stream = True)
-        progress_bar = tqdm(desc = filename_ext, unit = 'iB', unit_scale = True)
-        
-        for bytes in sfx_file.iter_content(BLOCK_SIZE):
-            progress_bar.update(len(bytes))
-            filepath.open('ab').write(bytes)
+        try:
+            filename     = sound.get('filename')
+            filename_ext = filename.split('/')[-1]
+
+            logger_prefix = f'{Fore.BLACK}{Back.WHITE}[Thread | {"%05d" % get_native_id()}]{Style.RESET_ALL}'
+            tqdm.write(f'{logger_prefix}{Fore.LIGHTBLUE_EX}{Style.DIM} Downloading \'{filename_ext}\'...{Style.RESET_ALL}')
+
+            with Path(args.output, filename_ext) as filepath:
+                filepath.parents[0].mkdir(parents = True, exist_ok = True)
+                filepath.open('wb').write(get(f'{DOMAIN}/{filename}').content)
+
+            tqdm.write(f'{logger_prefix}{Fore.LIGHTGREEN_EX}{Style.BRIGHT} Downloaded \'{filename_ext}\'!{Style.RESET_ALL}')
+
+        except Exception as _:
+            tqdm.write(f'{logger_prefix}{Fore.LIGHTYELLOW_EX} Download failed for \'{filename_ext}\'!{Style.RESET_ALL}')
+
+    thread_map(_download, loads(get(f'{DOMAIN}{args.soundlist}').text), max_workers = args.threads)
 
 if __name__ == '__main__':
     main()
