4e06378ce94653ff3fc6e76f5df773ee8bf6f467
Mason Hunt <masonhunt130ii@gmail.com>
2022-01-28 13:53:26 +1100

    Documentation + Gitignore Additions (Part 2)

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#--------------------------#
+# Created By  : Brittank88
+# Created Date: 28/01/2022
+# Version     : '1.0.0'
+# -------------------------#
+"""Python script to download all the SFX from https://gdcolon.com/🗿."""
+
 from argparse import ArgumentParser
 from json import loads
 from requests import get
@@ -8,14 +17,16 @@ BLOCK_SIZE = 1024   # 1 Kibibyte = 1024 bytes
 DOMAIN     = 'https://gdcolon.com'
 
 def init_argparse() -> ArgumentParser:
+    """Initialises and returns the argument parser."""
+
     parser = ArgumentParser(
         usage       = '%(prog)s [OPTION]',
-        description = f'Downloads all sound effects from {DOMAIN}/🗿.'
+        description = f'Downloads all SFX from {DOMAIN}/🗿.'
     )
     parser.add_argument(
         '-o', '--output'                 ,
         help    = 'The output directory.',
-        default = './out'
+        default = './sfx'
     )
     parser.add_argument(
         '-s', '--soundlist'                                    ,
@@ -25,6 +36,8 @@ def init_argparse() -> ArgumentParser:
     return parser
 
 def main() -> None:
+    """Main function to download SFX from Colon's server."""
+
     parser = init_argparse()
     args = parser.parse_args()
 
